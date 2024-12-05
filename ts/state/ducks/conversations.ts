5e71a73077bf6b5f61fd0ed5eca89fc07969ec35
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-12-05 09:55:11 +0100

    ts: Tweak conversation view for mobile phones

--- ts/state/ducks/conversations.ts
+++ ts/state/ducks/conversations.ts
@@ -4634,7 +4634,7 @@ type ShowConversationArgsType = ReadonlyDeep<{
 export type ShowConversationType = ReadonlyDeep<
   (options: ShowConversationArgsType) => unknown
 >;
-
+export { showConversation };
 function showConversation({
   conversationId,
   messageId,
@@ -4665,6 +4665,13 @@ function showConversation({
       dispatch(setComposerFocus(conversationId));
 
       return;
+    }
+
+    const navtabs = document.getElementsByClassName('NavTabs') as HTMLElement;
+    if (conversationId == undefined) {
+        navtabs[0].style.display = 'block';
+    } else {
+        navtabs[0].style.display = 'none';
     }
 
     // notify composer in case we need to stop recording a voice note
