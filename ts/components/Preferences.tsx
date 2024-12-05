9b5c863883136f62e3ba2ad6f54761451ae5395e
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-12-05 09:55:11 +0100

    ts: Tweak conversation view for mobile phones

--- ts/components/Preferences.tsx
+++ ts/components/Preferences.tsx
@@ -2211,7 +2211,7 @@ export function Preferences({
       <div className="module-title-bar-drag-area" />
       <div className="Preferences">
         <NavSidebar
-          title={i18n('icu:Preferences--header')}
+          title=''
           i18n={i18n}
           otherTabsUnreadStats={otherTabsUnreadStats}
           hasFailedStorySends={hasFailedStorySends}
--- ts/state/ducks/conversations.ts
+++ ts/state/ducks/conversations.ts
@@ -4704,7 +4704,7 @@ type ShowConversationArgsType = ReadonlyDeep<{
 export type ShowConversationType = ReadonlyDeep<
   (options: ShowConversationArgsType) => unknown
 >;
-
+export { showConversation };
 function showConversation({
   conversationId,
   messageId,
@@ -4735,6 +4735,13 @@ function showConversation({
       dispatch(setComposerFocus(conversationId));
 
       return;
+    }
+
+    const navtabs = document.getElementsByClassName('NavTabs') as HTMLElement;
+    if (conversationId == undefined) {
+        navtabs[0].style.display = 'block';
+    } else {
+        navtabs[0].style.display = 'none';
     }
 
     // notify composer in case we need to stop recording a voice note
