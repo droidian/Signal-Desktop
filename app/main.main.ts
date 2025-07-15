c381f8e72fea45b3e282aa9cd3dc7974bdefc73e
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2025-07-15 19:00:32 +0200

    app: Remove app updates check

--- app/main.main.ts
+++ app/main.main.ts
@@ -1161,31 +1161,6 @@ async function readyForUpdates() {
 
   // Discard value even if we don't handle a saved URL.
   macInitialOpenUrlRoute = undefined;
-
-  // Second, start checking for app updates
-  try {
-    strictAssert(
-      settingsChannel !== undefined,
-      'SettingsChannel must be initialized'
-    );
-    await updater.start({
-      canRunSilently: () => {
-        return (
-          systemTrayService?.isVisible() === true &&
-          mainWindow?.isVisible() !== true &&
-          !preventDisplaySleepService.isEnabled()
-        );
-      },
-      getMainWindow,
-      logger: updaterLog,
-      sql,
-    });
-  } catch (error) {
-    updaterLog.error(
-      'Error starting update checks:',
-      Errors.toLogFormat(error)
-    );
-  }
 }
 
 async function forceUpdate() {
