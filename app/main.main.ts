ba659d62c391140c18068c74945c5f1964515c0f
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2025-11-05 18:37:42 +0100

    app: Disable background throttling

--- app/main.main.ts
+++ app/main.main.ts
@@ -2292,6 +2292,7 @@ app.on('ready', async () => {
           nodeIntegration: false,
           sandbox: true,
           contextIsolation: true,
+          backgroundThrottling: false,
           preload: join(__dirname, '../bundles/loading/preload.preload.js'),
         },
         icon: windowIcon,
