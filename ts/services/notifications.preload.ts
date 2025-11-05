610ca9c80b1aea6e4b7118f76355412571ef43bc
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2025-11-05 18:25:56 +0100

    ts: services: notifications: Show one notification per conversation

--- ts/services/notifications.preload.ts
+++ ts/services/notifications.preload.ts
@@ -89,7 +89,6 @@ class NotificationService extends EventEmitter {
 
   public isEnabled = false;
 
-  #lastNotification: null | Notification = null;
   #notificationData: null | NotificationDataType = null;
   #tokenData: { token: string; data: NotificationClickData } | undefined;
 
@@ -199,13 +198,11 @@ class NotificationService extends EventEmitter {
         })
       );
     } else {
-      this.#lastNotification?.close();
-
       const notification = new window.Notification(title, {
         body: OS.isLinux() ? filterNotificationText(message) : message,
         icon: iconUrl,
         silent: true,
-        tag: messageId,
+        tag: conversationId
       });
 
       notification.onclick = () => {
@@ -234,8 +231,6 @@ class NotificationService extends EventEmitter {
           throw missingCaseError(type);
         }
       };
-
-      this.#lastNotification = notification;
     }
 
     if (!silent) {
@@ -348,9 +343,6 @@ class NotificationService extends EventEmitter {
         this.#tokenData = undefined;
         drop(window.IPC.clearAllWindowsNotifications());
       }
-    } else if (this.#lastNotification) {
-      this.#lastNotification.close();
-      this.#lastNotification = null;
     }
 
     // This isn't a boolean because TypeScript isn't smart enough to know that, if
