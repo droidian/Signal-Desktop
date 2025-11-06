7af2a604e637ff42e753750d8706ed93a0086e81
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2025-11-06 11:29:55 +0100

    ts: Tweak conversation view for mobile phones

--- ts/state/ducks/conversations.preload.ts
+++ ts/state/ducks/conversations.preload.ts
@@ -174,6 +174,7 @@ import type {
   SetQuotedMessageActionType,
   SetViewOnceActionType,
 } from './composer.preload.ts';
+import { saveDraftRecordingIfNeeded } from './composer.preload.ts';
 import {
   SET_FOCUS,
   replaceAttachments,
@@ -4824,7 +4825,7 @@ type ShowConversationArgsType = ReadonlyDeep<{
 export type ShowConversationType = ReadonlyDeep<
   (options: ShowConversationArgsType) => unknown
 >;
-
+export { showConversation };
 function showConversation({
   conversationId,
   messageId,
@@ -4883,6 +4884,30 @@ function showConversation({
         dispatch(scrollToMessage(conversationId, messageId));
       }
       dispatch(setComposerFocus(conversationId));
+
+      return;
+    }
+
+    const navtabs = document.getElementsByClassName('NavTabs') as HTMLElement;
+    if (conversationId == undefined) {
+        navtabs[0].style.display = 'block';
+    } else {
+        navtabs[0].style.display = 'none';
+    }
+
+    // notify composer in case we need to stop recording a voice note
+    if (
+      originalLocation.tab === NavTab.Chats &&
+      originalLocation.details.conversationId &&
+      originalLocation.details.conversationId !== conversationId
+    ) {
+      dispatch(saveDraftRecordingIfNeeded(originalLocation.details.conversationId));
+      dispatch(
+        onConversationClosed(
+          originalLocation.details.conversationId,
+          'showConversation'
+        )
+      );
     }
   };
 }
