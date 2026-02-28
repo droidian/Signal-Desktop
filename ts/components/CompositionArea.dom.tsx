fd95182388fd4bcce13440ac4283b7814d3cdbf5
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2026-02-28 23:15:35 +0100

    components: Move microphone button to menu

--- ts/components/CompositionArea.dom.tsx
+++ ts/components/CompositionArea.dom.tsx
@@ -456,6 +456,10 @@ export const CompositionArea = memo(function CompositionArea({
     }
   }, []);
 
+  const launchRecorder = useCallback(() => {
+      startRecording(conversationId);
+  }, []);
+
   const launchMediaPicker = useCallback(
     () => launchAttachmentPicker('media'),
     [launchAttachmentPicker]
@@ -796,19 +800,6 @@ export const CompositionArea = memo(function CompositionArea({
     </>
   );
 
-  const micButtonFragment = shouldShowMicrophone ? (
-    <div className="CompositionArea__button-cell">
-      <AudioCapture
-        conversationId={conversationId}
-        draftAttachments={draftAttachments}
-        i18n={i18n}
-        showToast={showToast}
-        warmupRecording={warmupRecording}
-        startRecording={startRecording}
-      />
-    </div>
-  ) : null;
-
   const editMessageFragment = draftEditMessage ? (
     <>
       {large && <div className="CompositionArea__placeholder" />}
@@ -851,6 +842,9 @@ export const CompositionArea = memo(function CompositionArea({
             </AxoDropdownMenu.Trigger>
           </div>
           <AxoDropdownMenu.Content>
+            <AxoDropdownMenu.Item symbol="message-badge" onSelect={launchRecorder}>
+              {i18n('icu:Keyboard--begin-recording-voice-note')}
+            </AxoDropdownMenu.Item>
             <AxoDropdownMenu.Item symbol="photo" onSelect={launchMediaPicker}>
               {i18n('icu:CompositionArea__AttachMenu__PhotosAndVideos')}
             </AxoDropdownMenu.Item>
@@ -1312,7 +1306,6 @@ export const CompositionArea = memo(function CompositionArea({
         )}
         {!isViewOnceActive && !large && (
           <>
-            {!dirty ? micButtonFragment : null}
             {editMessageFragment}
             {composerAddMenuButton}
           </>
@@ -1327,7 +1320,6 @@ export const CompositionArea = memo(function CompositionArea({
         >
           {leftHandSideButtonsFragment}
           {composerAddMenuButton}
-          {!dirty ? micButtonFragment : null}
           {editMessageFragment}
           {dirty || !shouldShowMicrophone ? sendButtonFragment : null}
         </div>
